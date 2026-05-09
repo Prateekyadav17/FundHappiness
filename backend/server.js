3d787eeff7268f386f0b1a3964b85e72d5bc15a2
Prateekyadav17 <prateek82250@gmail.com>
2026-05-09 22:12:03 +0530

    Fix Express 5 wildcard route for Render

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use('/images', express.static(path.join(__dirname, '../frontend/public/image
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-  app.get('*', (req, res) =>
+  app.get('(.*)', (req, res) =>
     res.sendFile(path.resolve(__dirname, '..', 'frontend', 'dist', 'index.html'))
   );
 } else {
